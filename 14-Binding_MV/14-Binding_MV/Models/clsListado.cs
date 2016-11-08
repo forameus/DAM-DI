12fd4b8e8bb80b2b3cbb30f6d5bccf693d22ef81
forameus <navarrogordilloalberto@gmail.com>
2016-11-08 13:39:35 +0100

    Hecho hasta ahora

---
+++ 14-Binding_MV/14-Binding_MV/Models/clsListado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace _14_Binding_MV.Models
+{
+    public class clsListado
+    {
+        public ObservableCollection<clsPersona> lista { get; set; }
+
+
+        public clsListado()
+        {
+            lista = new ObservableCollection<clsPersona>();
+            lista.Add(new clsPersona("Alberto", "Navarro", 1, new DateTime(1995,12,6), "Mi calle", "666 555 444"));
+            lista.Add(new clsPersona("Abradolf", "Lincler", 2, new DateTime(1945, 2, 15), "New Auschwitz", "666 666 123"));
+            //TODO: Añadir más personas
+
+        }
+    }
+}
---
+++ 14-Binding_MV/14-Binding_MV/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,23 @@
+﻿using _14_Binding_MV.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Binding_MV.ViewModels
+{
+    class clsMainPageVM
+    {
+        #region "Atributos"
+        public clsPersona _personaSeleccionada { get; set; }
+        public ObservableCollection<clsPersona> lista { get; set; }
+        #endregion
+
+        public clsMainPageVM()
+        {
+            lista = new clsListado().lista;
+        }
+    }
+}
