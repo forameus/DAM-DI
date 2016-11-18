eabcd46344c21edfcd0a52ae652237107ddba449
forameus <navarrogordilloalberto@gmail.com>
2016-11-18 14:39:58 +0100

    SII

---
+++ 15-DependencyPropertyYUserControls/15-DependencyPropertyYUserControls/UserControls/fichaAlumno.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace _15_DependencyPropertyYUserControls.UserControls
+{
+    public sealed partial class fichaAlumno : UserControl
+    {
+        public fichaAlumno()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+
+        public int Username
+        {
+            get { return (int)GetValue(UsernameProperty); }
+            set { SetValue(UsernameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Username.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UsernameProperty =
+            DependencyProperty.Register("Username", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+
+
+
+        public int Password
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
+
+
+
+    }
+}
