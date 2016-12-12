58d99c8cbace4ddb6a5226d3d0bec4dd0c362cdf
forameus <navarrogordilloalberto@gmail.com>
2016-12-12 14:39:23 +0100

    Añadido ejercicio de WPF

---
+++ 16-WPFPersonas/16-WPFPersonas-UI/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,96 @@
+﻿using _16_WPFPersonas_BL.Listados;
+using _16_WPFPersonas_BL.Manejadoras;
+using _16_WPFPersonas_ET;
+using _16_WPFPersonas_ET.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+
+namespace _16_WPFPersonas_UI.ViewModels
+{
+    public class clsMainPageVM : clsVMBase
+    {
+        #region "Atributos"
+        private clsPersona _personaSeleccionada;
+        public List<clsPersona> lista { get; set; }
+        
+        private DelegateCommand _eliminarCommand;
+        private DelegateCommand _guardarCommand;
+        private DelegateCommand _insertarPersona;
+        #endregion
+
+        public clsMainPageVM()
+        {
+            lista = new clsListadosBL().getListadoPersonaBL();            
+        }
+
+        public clsPersona Persona
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+
+            set
+            {
+                _personaSeleccionada = value;
+                _eliminarCommand.RaiseCanExecuteChanged();
+                NotifyPropertyChanged("Persona");
+            }
+        }
+
+
+
+      
+
+        public DelegateCommand eliminarCommand
+        {
+            get
+            {
+                _eliminarCommand = new DelegateCommand(EliminarCommand_Executed, EliminarCommand_CanExecute);
+                return _eliminarCommand;
+            }
+        }
+
+        private void EliminarCommand_Executed()
+        {
+            clsManejadoraPersonaBL mpbl = new clsManejadoraPersonaBL();
+            mpbl.deletePersona(_personaSeleccionada.id);
+
+            lista = new clsListadosBL().getListadoPersonaBL();
+            NotifyPropertyChanged("lista");
+        }
+
+        private bool EliminarCommand_CanExecute()
+        {
+            bool sePuedeBorrar = true;
+            if (_personaSeleccionada == null)
+                sePuedeBorrar = false;
+            return sePuedeBorrar;
+        }
+
+
+
+        public DelegateCommand guardarCommand
+        {
+            get
+            {                
+               _guardarCommand= new DelegateCommand(GuardarCommand_Executed);
+                return _guardarCommand;
+            }
+        }
+
+        private void GuardarCommand_Executed()
+        {
+            clsManejadoraPersonaBL mpbl = new clsManejadoraPersonaBL();
+            mpbl.updatePersona(_personaSeleccionada);
+
+            lista = new clsListadosBL().getListadoPersonaBL();
+            NotifyPropertyChanged("lista");
+        }
+
+    }
+}
---
+++ 16-WPFPersonas/16-WPFPersonas-UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using _16_WPFPersonas_UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _16_WPFPersonas_UI
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public clsMainPageVM ViewModel { get; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ViewModel = (clsMainPageVM)this.DataContext;
+        }
+
+        public void btnGuardar_Click(Object sender, RoutedEventArgs e)
+        {
+            txtNombre.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            txtApellidos.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            txtFechaNac.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            txtDireccion.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            lvPersonas.GetBindingExpression(ListView.ItemsSourceProperty).UpdateSource();
+        }
+    }
+}
